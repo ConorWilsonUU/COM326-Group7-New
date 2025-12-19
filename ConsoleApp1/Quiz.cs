77c42d8b100a6e92b2f9f5d7b62955f8957e0eb4
Conor Wilson <Wilson-C90@ulster.ac.uk>
2025-12-19 21:40:30 +0000

    I added my code from the pervious repo

--- ConsoleApp1/Quiz.cs
+++ ConsoleApp1/Quiz.cs
@@ -4,9 +4,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+public class Quiz
 {
-    internal class Quiz
+    private List<Question> quizQuestions;
+    private int quizID;
+    private string quizTitle;
+    private string quizDescription;
+    private DateTime quizDate;
+
+    public int QuizID
     {
+        get { return quizID; }
+        set { quizID = value; }
+    }
+    public string QuizTitle
+    {
+        get { return quizTitle; }
+        set { quizTitle = value; }
+    }
+    public string QuizDescription
+    {
+        get { return quizDescription; }
+        set { quizDescription = value; }
+    }
+    public DateTime QuizDate
+    {
+        get { return quizDate; }
+        set { quizDate = value; }
+    }
+
+    // Loads all questions
+    public Quiz()
+    {
+        quizQuestions = Question.GetAllQuestions();
+    }
+    // Constructor
+    public Quiz(int quizID, string quizTitle, string quizDescription, DateTime quizDate)
+    {
+        this.QuizID = quizID;
+        this.QuizTitle = quizTitle;
+        this.QuizDescription = quizDescription;
+        this.QuizDate = quizDate;
+
+        quizQuestions = Question.GetAllQuestions();
     }
 }
+
