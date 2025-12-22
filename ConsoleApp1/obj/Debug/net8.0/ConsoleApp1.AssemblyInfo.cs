d16f61a50449d9c0a4ce7b593c3468f70d56139b
Conor Wilson <Wilson-C90@ulster.ac.uk>
2025-12-22 13:43:12 +0000

    made tests

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c42d8b100a6e92b2f9f5d7b62955f8957e0eb4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+779b8a5f47e2db3ed85e384e02982208741a5903")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
